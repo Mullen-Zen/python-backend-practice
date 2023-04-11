d4cbc654e69fc008267877a42ceb97f561bc66eb
Garrison <mullenga@tcd.ie>
2023-04-11 22:39:59 +0100

    added multiple urls

--- hello/views.py
+++ hello/views.py
@@ -1,5 +1,23 @@
+import re
+from django.utils.timezone import datetime
 from django.http import HttpResponse
 
 # Create your views here.
 def home(request):
     return HttpResponse("Hello, Django!")
+
+def hello_there(request, name):
+    now = datetime.now()
+    formatted_now = now.strftime("%A, %d %B, %Y at %X GMT")
+
+    # Filter the name argument to letters only using regular expressions. URL arguments
+    # can contain arbitrary text, so we restrict to safe characters only.
+    match_object = re.match("[a-zA-Z]+", name)
+
+    if match_object:
+        clean_name = match_object.group(0)
+    else:
+        clean_name = "Friend"
+
+    content = "Hello, " + clean_name + "! It's currently " + formatted_now
+    return HttpResponse(content)
